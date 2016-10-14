8a1b116ba7e37b47f3421f95a3c4a1dce5595ba8
Doug Hughes <doug@theironyard.com>
2016-10-14 11:49:28 -0400

    add hello world template

---
+++ src/test/java/com/theironyard/HelloWorldTest.java
@@ -0,0 +1,40 @@
+package com.theironyard;
+
+import net.doughughes.testifier.annotation.Testifier;
+import net.doughughes.testifier.output.OutputStreamInterceptor;
+import net.doughughes.testifier.watcher.NotifyingWatcher;
+import net.doughughes.testifier.watcher.OutputWatcher;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+@Testifier(sourcePath = "./src/main/java/com/theironyard/HelloWorld.java", clazz = HelloWorld.class)
+public class HelloWorldTest {
+
+    @Rule
+    public NotifyingWatcher notifyingWatcher = new NotifyingWatcher("http://testifier.doughughes.net/notify");
+
+    @Rule
+    public OutputWatcher outputWatcher = new OutputWatcher();
+
+    @Test
+    @Testifier(method = "main", args = {String[].class})
+    public void testMain() {
+        /* arrange */
+
+        /* act */
+        // run the main method
+        HelloWorld.main(new String[]{});
+
+        /* assert */
+        OutputStreamInterceptor out = (OutputStreamInterceptor) System.out;
+        assertThat("No output was printed.",
+                out.getPrinted().size(), greaterThan(0));
+        assertThat("\"Hello World\" was not printed",
+                out.getPrinted().get(0), is("Hello World"));
+
+    }
+}
